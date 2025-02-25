ed02b309d3761e0ee415467132177ea9770afba7
NaitZapata <naittzapata@gmail.com>
2025-02-24 22:45:19 -0500

    initial commit

--- Vet/src/main/java/app/domain/models/Pet.java
+++ Vet/src/main/java/app/domain/models/Pet.java
@@ -9,22 +9,14 @@ import lombok.Setter;
 @NoArgsConstructor
 
 public class Pet {
-    private String name;
-    private long id;
-    private char race;
-    private int age;
-    private String specie;
-    private char features;
-    private int weigth;
+    private char name;
     private long ownerId;
 
-    public Pet(int weigth, long ownerId, char features, String specie, int age, char race, long id, String name) {
-        this.weigth = weigth;
+
+    public Pet(char name, long ownerId, int age, long id, char species, char breed, char color, double size, double weight, Timestamp dateCreated) {
+        this.name = name;
         this.ownerId = ownerId;
-        this.features = features;
-        this.specie = specie;
         this.age = age;
-        this.race = race;
         this.id = id;
         this.name = name;
     }
